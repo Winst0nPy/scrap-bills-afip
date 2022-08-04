ba4f55060b39c0e860b91a80c5980a33797e96c8
Daniel Cagliari <dacagliari@gmail.com>
2022-08-04 00:04:05 -0300

    adding functions to handler.

--- fitz_handler.py
+++ fitz_handler.py
@@ -2,16 +2,34 @@
 import math
 
 
-def print_page_blocks_by_keyword(page, keyword_to_search: str) -> None:
-    page_text = page.get_textpage()
-    text = page_text.extractBLOCKS()
-    for x in text:
+def print_page_blocks_by_keyword(blocks, keyword_to_search: str) -> None:
+    for x in blocks:
         x0, y0, x1, y1, word, block_no, block_type = x
         if keyword_to_search in word:
             text_to_show = f'y0:{math.floor(y0)} y1:{math.floor(y1)} x0:{math.floor(x0)} x1:{math.floor(x1)}, word: {repr(word)} '
             print(text_to_show)
 
 
+def print_page_blocks(blocks):
+    for x in blocks:
+        x0, y0, x1, y1, word, block_no, block_type = x
+        text_to_show = f'y0:{math.floor(y0)} y1:{math.floor(y1)} x0:{math.floor(x0)} x1:{math.floor(x1)}, word: {repr(word)} '
+        print(text_to_show)
+
+
+def round_all_coordinates_in_blocks(blocks):
+    return [(math.floor(x0), math.floor(y0), math.floor(x1), math.floor(y1), word, block_no, block_type)
+            for x0, y0, x1, y1, word, block_no, block_type in blocks]
+
+
 def get_page_block(page):
     return page.get_textpage().extractBLOCKS()
 
+
+def sort_page_block(blocks):
+    blocks.sort(key=lambda x: x[3])
+
+
+def join_block_by_coordinate(coordinate, blocks):
+    dictionary = {}
+    pass
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import glob
 import fitz
 from ScrapBillAFIP import ScrapBillAFIP
-import fitz_handler
+import fitz_handler as fh
 from config import *
 
 
@@ -11,8 +11,10 @@ def main():
     for file in source_files:
         with fitz.open(file) as doc:
             page = doc[0]
-            scraper = ScrapBillAFIP(fitz_handler.get_page_block(page))
-            fitz_handler.print_page_blocks_by_keyword(page, 'COD.')
+            block = fh.get_page_block(page)
+            fh.sort_page_block(block)
+            scraper = ScrapBillAFIP(block)
+            fh.print_page_blocks_by_keyword(page, 'COD.')
             scraper.print_page_block()
 
 
